18115670a6c309129dc29ab394586c60f866ec31
sameetah <102462831+sameetah@users.noreply.github.com>
2023-08-21 12:38:39 +0200

    Update Eagle.java

--- Eagle.java
+++ Eagle.java
@@ -1,6 +1,6 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
-    private boolean flying;
+    private  boolean flying;
     private int altitude;
 
     public Eagle(String name) {
@@ -17,8 +17,37 @@ public class Eagle extends Bird {
         return flying;
     }
 
-    @Override
     public String sing() {
         return "Screech!";
     }
+
+    public void takeOff() {
+        this.flying=true;
+        System.out.println(this.getName()+ " takes off in the sky");
+    }
+
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude += meters;
+            System.out.printf(this.getName()+" flies upwards, altitude: "+this.altitude);
+        }
+        return this.altitude;
+    }
+
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 1);
+            System.out.printf(this.getName()+" flies downward, altitude: "+this.altitude);
+        }
+        return altitude;
+    }
+
+    public void land() {
+        if (this.flying && (this.altitude <=9)) {
+            System.out.printf(this.getName()+ " lands on the ground");
+        } else {
+            System.out.printf(this.getName()+ " is too high, it cant land");
+        }
+    }
+
 }
